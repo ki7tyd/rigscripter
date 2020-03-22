b6a9a8ff353aae577c023ed4c827b7bb0d3f9219
KI7TYD <ki7tyd@outlook.com>
2020-03-22 10:59:57 -0700

    This now dumps a set config to a config file. Replace later with an actual auto config

--- autoconfigure.py
+++ autoconfigure.py
@@ -1,4 +1,13 @@
+import json
+import serial
+
+#config file TODO read this out of a master config file
+RIG_SERIAL_CONFIG_FILE = './rig_serialconfig.json'
+    
+
 def autoconfigure():
+    
+    #detect proper configuration TODO
     baudrates = [2400, 4800, 7200, 9600, 14400, 19200, 31250, 38400, 56000, 115200, 128000]
     parities = ['PARITY_NONE', 'PARITY_ODD', 'PARITY_EVEN']
     stopbits = ['STOPBITS_ONE', 'STOPBITS_ONE_POINT_FIVE', 'STOPBITS_TWO']
@@ -16,3 +25,18 @@ def autoconfigure():
                     print(f'Stopbits: {stopbit}')
                     print(f'Bytesize: {bytesize}')
                     print('\n')
+    
+#build serial config dictionary
+serial_config_dict = dict()
+serial_config_dict["port"] = "COM3"
+serial_config_dict["baudrate"] = "4800"
+serial_config_dict["parity"] = "N"
+serial_config_dict["stopbits"] = "2"
+serial_config_dict["bytesize"] = "8"
+serial_config_dict["timeout"] = "0.1"
+
+
+#write config to serial config file
+print("Writing to config file: {}".format(RIG_SERIAL_CONFIG_FILE))
+with open(RIG_SERIAL_CONFIG_FILE, 'w') as f:
+    json.dump(serial_config_dict, f)
