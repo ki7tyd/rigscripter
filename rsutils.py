3f3c4a554d09e84ff2af5a9543ac473180263a8f
KI7TYD <ki7tyd@outlook.com>
2020-03-22 16:38:58 -0700

    A bunch of utility functions

--- rsutils.py
+++ rsutils.py
@@ -1,12 +1,23 @@
 import serial
 import json
+import logging
 
-RIG_SERIAL_CONFIG_FILE = './rig_serialconfig.json'
+RS_CONFIG_FILE = './rs_config.json'
+
+#read global config file
+def read_rs_config_file():
+    #read config json
+    with open(RS_CONFIG_FILE) as file:
+        rs_config = json.load(file)
+    return rs_config
+
 
 #read config file and send back a configured serial object
 def get_serial():
     
-    RIG_SERIAL_CONFIG_FILE = './rig_serialconfig.json'
+    #RIG_SERIAL_CONFIG_FILE = './rig_serialconfig.json'
+    config = read_rs_config_file()
+    RIG_SERIAL_CONFIG_FILE = config["rig_serial_config_file"]
 
     #read config json
     with open(RIG_SERIAL_CONFIG_FILE) as file:
@@ -26,17 +37,43 @@ def get_serial():
     return ser
 
 
-#convert a string command to binary for sending to rig
-def cmd_to_bin():
-    return -1
+#Format a simple string and convert command to binary eg add ; at the end for sending to rig
+def cmdstr_to_cmdbin(cmd):
+    #check if it's a string
+    if type(cmd) == str:
+        #check that the last character is a ;, if not add one
+        if cmd[-1] != ';':
+            cmd = cmd + ';'
+        #convert to binary ascii
+        cmd = bytes(cmd, encoding="ascii")
+        return cmd
+
+    else:
+        return "Not a string"
 
 
 #convert a binary command to string for output or saving
-def bin_to_cmd():
-    return -1
+def cmdbin_to_cmdstr(cmd):
+    #check if it's binary
+    if type(cmd) == bytes:
+        cmd = cmd.decode('ascii')
+        return cmd
+    else:
+        return "Not binary"
 
 
 #send a command to the rig return the raw response
-def send_rig_cmd():
-    return -1
+def send_rig_cmd(cmd):
+    ser = get_serial()
+    if type(cmd) == str:
+        cmd = cmdstr_to_cmdbin()
+        send_rig_cmd(cmd)
+    elif type(cmd) == bytes:
+        ser.write(cmd)
+    else:
+        return "cmd wasn't a a string or bytes."
+    out = ser.readline()
+    ser.close()
+    return out
+
 
