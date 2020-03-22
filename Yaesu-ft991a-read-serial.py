181272f260462a0033aeb58916930e68d5c85076
KI7TYD <ki7tyd@outlook.com>
2020-03-22 11:01:57 -0700

    Just using this for testing. Uses new get_serial() function.

--- Yaesu-ft991a-read-serial.py
+++ Yaesu-ft991a-read-serial.py
@@ -4,19 +4,12 @@
 # http://www.varesano.net/blog/fabio/serial%20rs232%20connections%20python
 # https://www.yaesu.com/indexVS.cfm?cmd=DisplayProducts&ProdCatID=102&encProdID=D24F60F33816ED8BE5568D7E2B5E2131
 # https://www.yaesu.com/downloadFile.cfm?FileID=10604&FileCatID=158&FileName=FT%2D991%5FCAT%5FOM%5FENG%5F1612%2DD0.pdf&FileContentType=application%2Fpdf
-
+import json
 import serial
 import memorymanager
+import rsutils #project import
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-	port='COM3',
-	baudrate=4800,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_TWO,
-	bytesize=serial.EIGHTBITS,
-    timeout=0.1
-)
+ser = rsutils.get_serial()
 
 #commandarray = 	['FB147960000', 'FA146960000', 'FB']
 commandarray = ['MT001']
